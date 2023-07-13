0435d46d0bc3309967ecd7208e8211524e2dd10b
myusername <phanthaonhi1912@gmail.com>
2023-07-13 08:46:53 +0700

    complete

--- src/sql/creat_db.sql
+++ src/sql/creat_db.sql
@@ -10,13 +10,6 @@ CREATE TABLE ta_lpn_user(
     IsOnline int DEFAULT 0,
     IsPlaying int DEFAULT 0
 );
-CREATE TABLE ta_lpn_friend(
-    I_ID_User1 int NOT NULL,
-    I_ID_User2 int NOT NULL,
-    FOREIGN KEY (I_ID_User1) REFERENCES `ta_lpn_user`(I_ID),
-    FOREIGN KEY (I_ID_User2) REFERENCES `ta_lpn_user`(I_ID),
-    CONSTRAINT PK_ta_lpn_friend PRIMARY KEY (I_ID_User1,I_ID_User2)
-);
 CREATE TABLE ta_lpn_banned_user(
     I_ID_User int PRIMARY KEY NOT NULL,
     FOREIGN KEY (I_ID_User) REFERENCES `ta_lpn_user`(I_ID)
